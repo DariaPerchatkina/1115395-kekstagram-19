2d168a07979f0acebaa99faa4a4338916c5b9ca2
Дарья <perchatkinadaria@gmail.com>
2020-02-13 17:51:33 +0300

    создает массив первоначальных данных

---
+++ js/main.js
@@ -0,0 +1,92 @@
+"use strict";
+
+var USER_NAME = ['Петька', 'Максим', 'Аня', 'Лёля', 'Артем', 'Саша', 'Костя', 'Ира']; // создает массив пользователей
+var USERS_MESSAGE = [ // создает массив с комментариями
+    'Всё отлично!', 
+    'В целом всё неплохо. Но не всё.', 
+    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 
+    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+    ];
+var COUNT = 25;
+var likes = []
+
+var similarWizardTemplate = document.querySelector('#picture') // находим темплейт в разметке
+.content // берем все содержимое дива
+.querySelector('.picture');
+
+var getRandomValueArr = function (arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+};
+
+// // создание мага из рандомных данных и добавление в пустой массив wizardsRandom
+var photoRandomCreate = function (count) { // создаем функцию, которая будет генерировать случайных набор фото
+  var photoArr = []; // делаем пустой массив данных
+
+  for (var i = 0; i < count; i++) { // условия работы цикла
+    var url = getRandomValueArr(photos/[i].jpg); // обьявим переменную для генерации имен волшебников
+    var  discription = getRandomValueArr(); // добавим  переменную для генерации рандомных цветов плащей
+    var likes = getRandomValueArr(); // добавим переменную для генерации рандомных цветов глаз
+    var comments = [avatar, message, name];
+    photoArr.push({url: url, 
+        description: discription,// создадим обьект и при помощи push добавим его в массив arr(в нашем случае это пустой массив wizardsRandom)
+        likes: likes,
+        comments: comments
+    });
+  }
+  return photoArr;
+};
+
+// // находим и показываем окно настроек пользователя
+
+// var userDialog = document.querySelector('.setup'); // находит по классу разметке div с модальным окном
+// userDialog.classList.remove('hidden'); // удаляет класс hidden
+
+// document.querySelector('.setup-similar').classList.remove('hidden'); // отображает поле в модалке, где находятся 4 мага
+
+// var similarListElement = document.querySelector('.setup-similar-list'); // находит список пока что одинаковых магов
+
+
+// var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+// var WIZARD_LASTNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+// var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210 ,55)', 'rgb(0, 0 ,0)'];
+// var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+// var WIZARD_COUNT = 4;
+
+// // необходимо написать функцию, которая позволит создавать рандомную связку имя-фамилия для магов из представленных массивом имен и фамилий
+// // каждая составляющая рандомного имени массива будет находится при помощи поиска рандомного значения,а итоговое имя мага будет получаться при попмощт конкатенации
+// var getRandomValueArr = function (arr) {
+//   return arr[Math.floor(Math.random() * arr.length)];
+// };
+
+// var wizardFullName = function (name, lastName) {
+//   var wizardsName = getRandomValueArr(name);
+//   var wizardsLastName = getRandomValueArr(lastName);
+//   return wizardsName + ' ' + wizardsLastName;
+// };
+
+
+
+// var wizards = wizardsRandomCreate(WIZARD_COUNT);
+
+var renderWizard = function (wizard) { // создаем функцию, не придумала пока как обьяснить для чего она
+  var wizardElement = similarWizardTemplate.cloneNode(true); // делаем дубликат узла template
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name; // находим в ДОМ div c классом .setup-similar-label и задаем ему текстовое содержимое
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor; // по аналогии с цветами
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  return wizardElement; // возвращаем полученный склонированный элемент с новым содержимым
+};
+
+var renderWizards = function (wizardsElem) {
+  var fragment = document.createDocumentFragment(); // создаем пустой объект DocumentFragment
+  for (var i = 0; i < wizardsElem.length; i++) { // условия работы цикла, идет переборка массива случайно созданных волшебников
+    fragment.appendChild(renderWizard(wizardsElem[i])); // добавляет созданного волшебника во фрагмент
+  }
+  similarListElement.appendChild(fragment); // добавляет фрагмент в разметку
+};
+renderWizards(wizards);
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden'); // отключает класс hidden у окна,отображающего сгененрированнвх волшебников в модалке
